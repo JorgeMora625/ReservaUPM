79bb6fd22c6d142e6c2f4f15628efc06ac5691b7
JorgeMora625 <jorgemoracollado@gmail.com>
2024-12-26 19:33:06 +0100

    mas tests

--- base/tests.py
+++ base/tests.py
@@ -2,11 +2,13 @@ from django.test import TestCase
 from .models import *
 
 # Models tests--------------------------------------------------------------------------------------------------------------------------
+
+
 class AlumnoTestCase(TestCase):
     def setUp(self):
         User.objects.create(username="prueba", first_name="prueba", last_name="de alumno", email="prueba@alumnos.upm.es", password="prueba")
 
-    def test_crea_alumno(self):
+    def crea_alumno(self):
         usuario = User.objects.get(last_name="de alumno")
         id_user = usuario.id
         alumno = Alumnos.objects.get(nombre="prueba")
@@ -18,7 +20,7 @@ class ProfesorTestCase(TestCase):
     def setUp(self):
         User.objects.create(username="prueba", first_name="prueba", last_name="de profesor", email="prueba@upm.es", password="prueba")
 
-    def test_crea_profesor(self):
+    def crea_profesor(self):
         usuario = User.objects.get(last_name="de profesor")
         id_user = usuario.id
         profesor = Profesores.objects.get(nombre="prueba")
