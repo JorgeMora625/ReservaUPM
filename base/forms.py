e82748c866c7954b1318033c87abbbeb4c266df5
JorgeMora625 <jorgemoracollado@gmail.com>
2025-01-04 21:17:25 +0100

    cambios

--- base/forms.py
+++ base/forms.py
@@ -56,7 +56,7 @@ class RegistroForm(UserCreationForm):
          user.username = self.cleaned_data['username']
          user.first_name = self.cleaned_data['first_name']
          user.last_name = self.cleaned_data['last_name']
-         user.email = self.clean_correo()  
+         user.email = self.clean_email()  
          if commit:
             user.save()
          return user
